4e8826f5799e335a840799574e590e352cfa22ce
Kaickg07 <gomeskaick54@gmail.com>
2026-01-28 10:26:37 -0300

    Projeto Final POO C#

--- ATVFinal.cs
+++ ATVFinal.cs
@@ -90,14 +90,56 @@ namespace ATVFinal
             Console.WriteLine("BEM VINDO A BIBLIOTECA");
 
 
-            Cliente meucliente = new Cliente("Kaick Gomes", "143.976.546-43", "(83)4002-8922", "Rua João Pessoa", "Kaickgomes3@gmail.com");
-            Livro meulivro = new Livro("Fogo e sangue", "R.R Martin", 2018, "9788556510761", "Suma", "Disponível");
-            Emprestimo meuemprestimo = new Emprestimo(meulivro, meucliente, 1);
+            Cliente meucliente1 = new Cliente("Kaick Gomes","143.976.546-43","(83)4002-8922","Rua João Pessoa","Kaickgomes3@gmail.com");
+            Cliente meucliente2 = new Cliente("Enzo Silveira","254.891.032-15","(81)3221-4005","Rua das Flores","Enzosilveira8@gmail.com");
+            Cliente meucliente3 = new Cliente("Lívia Medeiros","762.145.980-22","(84)98877-1234","Avenida Brasil","Liviamedeiros.contato@gmail.com");
+            Cliente meucliente4 = new Cliente("Thiago Cavalcanti","109.334.657-88","(83)3244-1010","Rua Duque de Caxias","Thicavalcanti99@gmail.com");
+            Cliente meucliente5 = new Cliente("Beatriz Viana","445.009.123-56","(11)4004-0000","Alameda dos Anjos","Biaviana.vendas@gmail.com");
+            Cliente meucliente6 = new Cliente("Matheus Rocha","918.273.645-09","(21)2506-7000","Rua Sete de Setembro","Matheusrocha_dev@gmail.com");
+            Cliente meucliente7 = new Cliente("Camila Antunes","332.556.778-11","(31)3409-5000","Avenida Getúlio Vargas","Camilantunes22@gmail.com");
+            Cliente meucliente8 = new Cliente("Felipe Dantas","554.120.339-44","(85)3366-7300","Rua Major Facundo","Felipe.dantas.work@gmail.com");
+            Cliente meucliente9 = new Cliente("Isabela Pontes","667.889.001-37","(71)3115-1111","Rua da Paciência","Isabelapontes.office@gmail.com");
+            Cliente meucliente10 = new Cliente("Gustavo Lins","002.443.556-28","(61)3212-1000","Eixo Monumental","Gustavolins.pro@gmail.com");
+            
+            Livro meulivro1 = new Livro("Fogo e sangue","R.R Martin",2018,"9788556510761","Suma","Disponível");
+            Livro meulivro2 = new Livro("Harry Potter e a Pedra Filosofal","J.K. Rowling",1997,"9788532530783","Bloomsbury","Disponível");
+            Livro meulivro3 = new Livro("O Diário de Anne Frank","Anne Frank",1947,"9788501044457","Contact Publishing","Indisponível");
+            Livro meulivro4 = new Livro("Fogo e sangue","R.R Martin",2018,"9788556510761","Suma","Disponível");
+            Livro meulivro5 = new Livro("O Diário de Anne Frank","Anne Frank",1947,"9788501044457","Contact Publishing","Disponível");
+            Livro meulivro6 = new Livro("Harry Potter e a Pedra Filosofal","J.K. Rowling",1997,"9788532530783","Bloomsbury","Disponível");
+            Livro meulivro7 = new Livro("Fogo e sangue","R.R Martin",2018,"9788556510761","Suma","Disponível");
+            Livro meulivro8 = new Livro("Dom Quixote","Miguel de Cervantes",1605,"9788424115456","Francisco de Robles","Indisponível");
+            Livro meulivro9 = new Livro("Fogo e sangue","R.R Martin",2018,"9788556510761","Suma","Disponível");
+            Livro meulivro10 = new Livro("Dom Quixote","Miguel de Cervantes",1605,"9788424115456","Francisco de Robles", "Indisponível");
+            Emprestimo meuemprestimo1 = new Emprestimo(meulivro2,meucliente5,10);
+            Emprestimo meuemprestimo2 = new Emprestimo(meulivro3,meucliente1,1);
+            Emprestimo meuemprestimo3 = new Emprestimo(meulivro4,meucliente7,3);
+            Emprestimo meuemprestimo4 = new Emprestimo(meulivro7,meucliente2,5);
+            Emprestimo meuemprestimo5 = new Emprestimo(meulivro6,meucliente3,20);
+            Emprestimo meuemprestimo6 = new Emprestimo(meulivro1,meucliente4,0);
+            Emprestimo meuemprestimo7 = new Emprestimo(meulivro5,meucliente8,1);
+            Emprestimo meuemprestimo8 = new Emprestimo(meulivro9,meucliente6,0);
+            Emprestimo meuemprestimo9 = new Emprestimo(meulivro10,meucliente9,7);
+            Emprestimo meuemprestimo10 = new Emprestimo(meulivro8,meucliente10,4);
 
-
-            Console.WriteLine(meucliente.ListarCliente());
-            Console.WriteLine(meulivro.ListarLivro());
-            Console.WriteLine(meuemprestimo.ListarEmprestimos());
+            Console.WriteLine("CLIENTES CADASTRADOS:");
+            Cliente [] todosClientes = {meucliente1,meucliente2,meucliente3,meucliente4,meucliente5,meucliente6,meucliente7,meucliente8,meucliente9,meucliente10};
+            foreach (var c in todosClientes)
+            {
+                Console.WriteLine(c.ListarCliente());
+            }
+            Console.WriteLine("LIVROS CADASTRADOS:");
+            Livro [] todosLivros = {meulivro1,meulivro2,meulivro3,meulivro4,meulivro5,meulivro6,meulivro7,meulivro8,meulivro9,meulivro10};
+            foreach (var l in todosLivros)
+            {
+                Console.WriteLine(l.ListarLivro());
+            }
+            Console.WriteLine("LISTA DE EMPRESTIMOS:");
+            Emprestimo [] todosEmprestimos = {meuemprestimo1,meuemprestimo2,meuemprestimo3,meuemprestimo4,meuemprestimo5,meuemprestimo6,meuemprestimo7,meuemprestimo8,meuemprestimo9,meuemprestimo10};
+            foreach (var e in todosEmprestimos)
+            {
+                Console.WriteLine(e.ListarEmprestimos());
+            }
 
         }
     }
